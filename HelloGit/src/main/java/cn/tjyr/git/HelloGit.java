8c70a4a6a1fa3b8a3761754b2a25d5e0e3bfa846
weiwutan <weiwt2019@qq.com>
2020-06-04 12:20:48 +0800

    拉取后修改

--- HelloGit/src/main/java/cn/tjyr/git/HelloGit.java
+++ HelloGit/src/main/java/cn/tjyr/git/HelloGit.java
@@ -7,5 +7,6 @@ public class HelloGit {
     System.out.println("更新一");
     System.out.println("创建分支");
     System.out.println("在线更新");
+    System.out.println("再次更改");
   }
 }
---
+++ HelloGit/src/main/java/cn/tjyr/git/HelloGit01.java
@@ -0,0 +1,8 @@
+package cn.tjyr.git;
+
+public class HelloGit01 {
+  public static void main(String[] args) {
+    // 测试git
+    System.out.println("测试git");
+  }
+}
